dbc6dfe6de6d065647db85fec59c420286e58451
lizafedoseeva734-ship-it <lizafedoseeva734@gmail.com>
2026-07-21 00:28:28 +0300

    hw8 completed

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,14 +1,15 @@
 import HW5 from '../s2-homeworks/hw05/HW5';
 import HW6 from '../s2-homeworks/hw06/HW6';
 import HW7 from '../s2-homeworks/hw07/HW7';
+import HW8 from '../s2-homeworks/hw08/HW8';
 import s from './App.module.css';
 
 function App() {
   return (
     <div className={s.App}>
       <HW5 />
-      <HW6 />
       <HW7 />
+      <HW8 />
       {/* <HW1 />
 
             <HW2/>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  switch (action.type) {
+    case 'sort': {
+      const copyState = [...state];
 
-            return state // need to fix
+      copyState.sort((a, b) => {
+        if (action.payload === 'up') {
+          return a.name.localeCompare(b.name, 'ru');
+        } else {
+          return b.name.localeCompare(a.name, 'ru');
         }
-        case 'check': {
+      });
 
-            return state // need to fix
-        }
-        default:
-            return state
+      return copyState;
     }
-}
+    case 'check': {
+      return state.filter((user) => user.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
