43749182c0b6a864f2f640faba93fe2d966ffdee
lizafedoseeva734-ship-it <lizafedoseeva734@gmail.com>
2026-07-28 21:19:06 +0300

    hw9 completed fix

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,22 +1,11 @@
 import HW5 from '../s2-homeworks/hw05/HW5';
-import HW6 from '../s2-homeworks/hw06/HW6';
-import HW7 from '../s2-homeworks/hw07/HW7';
-import HW8 from '../s2-homeworks/hw08/HW8';
-import HW9 from '../s2-homeworks/hw09/HW9';
+
 import s from './App.module.css';
 
 function App() {
   return (
     <div className={s.App}>
       <HW5 />
-      <HW7 />
-      <HW8 />
-      <HW9 />
-      {/* <HW1 />
-
-            <HW2/>
-            <HW3 />
-            <HW4 /> */}
 
       {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
     </div>
