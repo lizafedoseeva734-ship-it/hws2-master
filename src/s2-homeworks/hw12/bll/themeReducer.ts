c0aee252dbe6db969e41361d24cf172f68f99876
lizafedoseeva734-ship-it <lizafedoseeva734@gmail.com>
2026-08-26 18:07:33 +0300

    hw12 completed

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,32 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+// Типизируем стейт автоматически на основе initState
+export type State = typeof initState;
 
-        default:
-            return state
-    }
-}
+// Типизируем экшен
+export type Action = {
+  type: 'SET_THEME_ID';
+  id: number;
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+// 1 - Написали нужные типы вместо any, дописали редюсер
+export const themeReducer = (state: State = initState, action: Action): State => {
+  switch (action.type) {
+    case 'SET_THEME_ID':
+      return {
+        ...state,
+        themeId: action.id,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// Исправили any на возвращаемый тип Action
+export const changeThemeId = (id: number): Action => ({
+  type: 'SET_THEME_ID',
+  id,
+});
